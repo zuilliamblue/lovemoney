fa868025c52be6f68c9460c8f6ce42203883ee75
zuilliamblue <wmgcomercial@gmail.com>
2025-06-23 15:33:29 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-export default function HomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Bem-vindo ao LoveMoney!</h1>
-    </div>
-  );
-}
